0297dd98209ce64711e35ac960df42c397c0507d
Laura Wilhelmi <laurawilhelmi.pro@gmail.com>
2023-01-27 16:08:48 +0100

    all

--- sql_basics.sql
+++ sql_basics.sql
@@ -26,3 +26,13 @@ SELECT * FROM students
 WHERE school LIKE 1;
 
 -- Affiche uniquement les prénoms des étudiants, par ordre inverse à l’alphabet (DESC). Ensuite, la même chose mais en limitant les résultats à 2.
+SELECT prenom FROM students ORDER BY prenom DESC;
+
+SELECT prenom FROM students ORDER BY prenom DESC LIMIT 2;
+
+--Ajoute Ginette Dalor, née le 01/01/1930 et affecte-la à Bruxelles, toujours en SQL.
+INSERT INTO `students` ( `nom`, `prenom`, `datenaissance`, `genre`, `school`) VALUES
+('Dalor', 'Ginette', '1930-01-01', 'F', 1);
+
+--Modifie Ginette (toujours en SQL) et change son sexe et son prénom en “Omer”.
+
